1a817583f193657c57b523299ccb8c7a371b5679
Alex Dollard <alexdollard141@gmail.com>
2020-01-21 12:43:36 -0600

    post request working; notes rendering on page.

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -12,19 +12,7 @@ console.log("noteData");
 
 module.exports = function(app) {
 
-    //get request to the database, which sends back the data as JSON format
-    // app.get("/api/notes", function (req, res) {
-    //      fs.readFile("../db/db.json", (err, data) => {
-    //         if(err){
-    //             console.log(err)
-    //         }
-    //         console.log(data);
-            
-    //         res.json(data);
-    //     });
-    //   console.log("what now?")
-    // res.JSON(noteData); 
-    // });
+
 
     app.get("/api/notes", function(req, res) {
         fs.readFile("./db/db.json",  (err, data) => {
@@ -36,37 +24,26 @@ module.exports = function(app) {
 
 
 
-
-
     app.post("/api/notes", function(req, res) {
-        console.log(req.body);
-        res.send("Thanks!")
+
+        let incomingNote = req.body
+
+        fs.readFile("./db/db.json", (err, data) => {
+            if(err) throw err;
+            parsedData = JSON.parse(data);
+            parsedData.push(incomingNote);
+            stringData = JSON.stringify(parsedData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+                })
+        })
+
+        res.send("Thank you for your note!");
+
     })
 
 
-    // app.post("/api/notes", function (req, res) {
 
-    //     console.log(req.body)
-    //     let note = req.body
-
-    //     let notesArray = []; 
-
-    //     fs.readFile("../db/db.json",  (err, data) => {
-    //         if (err) throw err;
-    //         console.log(JSON.parse(data));
-    //         notesArray = JSON.parse(data);
-    //         notesArray.push(note);
-
-
-    //         res.JSON(note);
-
-    //         fs.writeFile("../db/db.json", JSON.stringify(notesArray, null, 2), (err) => {
-    //             if(err){
-    //                 console.log(err)
-    //             }           
-    //         });            
-    //    });
-       
-    // })
 
 }
