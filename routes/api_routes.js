dbccf3f2560c7b5ce4141860cffc380cf4c862a4
Alex Dollard <alexdollard141@gmail.com>
2020-01-21 15:20:07 -0600

    mostly complete delete request

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -50,7 +50,30 @@ module.exports = function(app) {
     })
 
 
+    app.delete("/api/notes/:id", function(req, res) {
+        //gets the id# of the note set for deletion
+        let chosen_for_death = req.params.id
+        console.log(chosen_for_death);
 
+        //reads the current database file
+        fs.readFile("./db/db.json", (err, data) => {
+            if(err) throw err;
+            //parses current database file into array of objects
+            parsedData = JSON.parse(data);
+            //for each function, comparing each note's id to the chosen_for_death variable
+            for(let i=0; i < parsedData.length; i++){
+                if(parsedData[i].id === Number(chosen_for_death)){
+                    parsedData.splice([i], 1);
+                }
+            }
+            console.log(parsedData);
+            stringData = JSON.stringify(parsedData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+                })
+        })
+    })
 
 
 
